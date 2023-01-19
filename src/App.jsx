abcf84d9b245683f559fd4142fea699d95bb007e
Minh Quan Do <minhquan98vn@gmail.com>
2023-01-19 13:27:39 +0700

    ✨ feat: Set up layout of the app and add useLocalStorage hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+import { useLocalStorage } from './hooks';
+import { Header, Body, Footer } from './components';
 
 const App = () => {
+  const [type, setType] = useState('basics');
+  const [score, setScore] = useLocalStorage('score', 0);
+
+  useEffect(() => {
+    setScore(0);
+  }, [type]);
+
   return (
-    <div>App</div>
+    <>
+      <Header type={type} score={score} />
+      <Body type={type} setScore={setScore} />
+      <Footer type={type} setType={setType} />
+    </>
   )
 }
 
